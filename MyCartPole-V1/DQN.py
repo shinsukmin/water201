1495e0c28e7cc58414f8d214b02774b6634b7950
DESKTOP-0RH78CA\sbh13 <sukmin.shin@playlinks.com>
2022-02-22 17:29:01 +0900

    Cartpole My version complete

---
+++ MyCartPole-V1/DQN.py
@@ -0,0 +1,90 @@
+# DeepLearningFlappyBird 코드를 내임맛에 맞게 수정해보자.
+# 
+# 
+# 
+# 
+import tensorflow as tf
+import numpy as np
+
+GAME = 'bird' # the name of the game being played for log files
+ACTIONS = 2 # number of valid actions
+GAMMA = 0.99 # decay rate of past observations
+OBSERVE = 5000. # timesteps to observe before training
+EXPLORE = 50000. # frames over which to anneal epsilon
+FINAL_EPSILON = 0.00001 # final value of epsilon
+INITIAL_EPSILON = 0.00001 # starting value of epsilon
+REPLAY_MEMORY = 50000 # number of previous transitions to remember
+BATCH = 32 # size of minibatch
+FRAME_PER_ACTION = 1
+USE_CHECK_POINT = True
+
+class DQN():
+    def __init__(self, sess, name, input_size, output_size):
+        self.sess = sess
+        self.net_name = name
+        self.input_size = input_size
+        self.output_size = output_size
+        self.learning_rate = 0.001
+
+        self.createNetwork()
+
+    # truncate_normal: 절단정규분포로부터의 난수값을 반환합니다.
+    # stddev: 0-D 텐서 또는 파이썬 값. 절단정규분포의 표준 편차.
+    # https://tensorflowkorea.gitbooks.io/tensorflow-kr/content/g3doc/api_docs/python/constant_op.html
+    # shape 형태의 행렬을 정규분포의 난수로 채워넣음.
+    @staticmethod
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev = 0.01)
+        return tf.Variable(initial)
+
+    @staticmethod
+    def bias_variable(shape):
+        initial = tf.constant(0.01, shape = shape)
+        return tf.Variable(initial)
+
+
+    def createNetwork(self):
+        with tf.variable_scope(self.net_name):
+            self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
+        
+            W1 = tf.Variable(tf.truncated_normal([self.input_size, 64], stddev = 0.01), name = "w1")
+            b1 = tf.Variable(tf.truncated_normal(shape = [64]), name = "b1")
+            Layer1 = tf.nn.tanh(tf.matmul(self._X, W1) + b1)
+            # Layer1 = tf.nn.relu(tf.matmul(self._X, W1))
+
+            W2 = tf.Variable(tf.truncated_normal([64, 32], stddev = 0.01), name = "w2")
+            b2 = tf.Variable(tf.truncated_normal(shape = [32]), name = "b2")
+            Layer2 = tf.nn.relu(tf.matmul(Layer1, W2) + b2)
+            # Layer2 = tf.nn.relu(tf.matmul(Layer1, W2))
+
+            W3 = tf.Variable(tf.truncated_normal([32, 16], stddev = 0.01), name = "w3")
+            b3 = tf.Variable(tf.truncated_normal(shape = [16]), name = "b3")
+            Layer3 = tf.nn.relu(tf.matmul(Layer2, W3) + b3)
+            # Layer3 = tf.nn.relu(tf.matmul(Layer2, W3))
+
+            W4 = tf.Variable(tf.truncated_normal([16, self.output_size], stddev = 0.01), name = "w4")
+            self._QPred = tf.matmul(Layer3, W4)
+
+            self._Y = tf.placeholder(tf.float32, shape=[None], name="_Y")
+            self._A = tf.placeholder(tf.float32, [None, self.output_size], name="_A")
+            
+            self._QValue = tf.reduce_sum(tf.multiply(self._QPred, self._A), reduction_indices=1, name="_QValue")
+
+            # loss function
+            self._loss = tf.reduce_mean(tf.square(self._Y - self._QValue))
+            # learning
+            self._train = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self._loss)
+
+    
+    def predict(self, state):
+        x = np.reshape(state, [1, self.input_size])
+        return self.sess.run(self._QPred, feed_dict={self._X: x})
+        
+
+    def update(self, s_batch, r_batch, a_batch):
+        return self.sess.run([self._loss, self._train], feed_dict={self._X: s_batch, self._Y: r_batch, self._A: a_batch})
+
+
+
+        
+
---
+++ MyCartPole-V1/main.py
@@ -0,0 +1,133 @@
+# DeepLearningFlappyBird 코드를 내임맛에 맞게 수정해보자.
+# 
+# 
+# 
+# 
+import tensorflow as tf
+import sys
+import gym
+from collections import deque
+import random
+
+from DQN import DQN
+sys.path.append("game/")
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_copy_var_ops(dest_scope_name="target", src_scope_name="main"):
+    op_holder = []
+
+    src_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope_name)
+    dest_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope_name)
+
+    for src_var, dest_var in zip(src_vars, dest_vars):
+        op_holder.append(dest_var.assign(src_var.value()))
+    return op_holder
+
+env = gym.make('CartPole-v1')
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+
+discount_factor = 0.99
+epsilon = 0.4
+epsilon_decay = 0.999
+epsilon_min = 0.01
+batch_size = 64
+train_start = 200
+memory = deque(maxlen=2000)
+
+sess = tf.InteractiveSession()
+mainDQN = DQN(sess, "main", state_size, action_size)
+targetDQN = DQN(sess, "target", state_size, action_size)
+
+sess.run(tf.initialize_all_variables())
+
+copy_ops = get_copy_var_ops(dest_scope_name="target", src_scope_name="main")
+
+def choose_action(state):
+    if (np.random.rand() <= epsilon):
+        return random.randrange(action_size)
+    else:
+        return np.argmax(mainDQN.predict(state))
+
+def remember(state, action, reward, next_state, done):
+    memory.append((state, action, reward, next_state, done))
+
+def trainModel():
+    global epsilon
+    if (epsilon > epsilon_min):
+        epsilon = epsilon * epsilon_decay
+
+    s_batch = []
+    r_stack = []
+    a_batch = []
+    
+    mini_batch = random.sample(memory, batch_size)
+
+    for state, action, reward, next_state, done in mini_batch:
+        if done:
+            r_stack.append(reward)
+        else:
+            r_stack.append(reward + discount_factor * np.max(targetDQN.predict(next_state)))
+
+        s_batch.append(state)
+        a_onehot = [0, 0]
+        a_onehot[action] = 1
+        a_batch.append(a_onehot)
+
+    return mainDQN.update(s_batch, r_stack, a_batch)
+
+
+
+def playGame():
+
+    scores, episodes = [], []
+    score_avg = 0
+    # episode 가 시작될 때마다 환경을 초기화한다.
+    num_episode = 3000
+    for e in range(num_episode):
+        done = False
+        score = 0
+
+        state = env.reset()
+
+        # 현재 상태에서 action 을 하나 선택하여 한 스텝 진행한다.
+        # 그 결과로 받은 보상을 현재 상태와 선택한 행동과 함께 리플레이 메모리에 저장한다.
+        # 리플레이 메모리가 일정 크기 이상으로 저장되면 매 스텝마다 학습할 수 있도록 한다.
+        while not done:
+            env.render()
+
+            action = choose_action(state)
+
+            next_state, reward, done, info = env.step(action)
+
+            score += reward
+            if not done or score == 500:
+                reward = 0.1
+            else:
+                reward = -1
+
+            remember(state, action, reward, next_state, done)
+            if len(memory) >= train_start:
+                loss, _ = trainModel()
+                # print('loss: {}', loss)
+
+            state = next_state
+
+            if done:
+                sess.run(copy_ops)
+                if score_avg == 0:
+                    score_avg = score
+                else:
+                    score_avg = 0.9 * score_avg + 0.1 * score
+                
+                print('episode: {:3d} | score avg {:3.2f} | memory length: {:4d} | epsilon: {:.4f}'.format(e, score_avg, len(memory), epsilon))
+
+                scores.append(score_avg)
+                episodes.append(e)
+                plt.plot(episodes, scores, 'b')
+                plt.xlabel('episode')
+                plt.ylabel('average score')
+                plt.savefig('cartpole_graph.png')
+    
+playGame()
