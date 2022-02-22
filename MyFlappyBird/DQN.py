4764724276eb7ec1ca5eae7ea608f28677f220b3
DESKTOP-0RH78CA\sbh13 <sukmin.shin@playlinks.com>
2022-02-22 13:52:38 +0900

    ...

--- MyFlappyBird/DQN.py
+++ MyFlappyBird/DQN.py
@@ -18,10 +18,15 @@ BATCH = 32 # size of minibatch
 FRAME_PER_ACTION = 1
 USE_CHECK_POINT = True
 
-print(np.__version__)
 class DQN():
-    def __init__(self):
-        print('Do nothing')
+    def __init__(self, sess, name, input_size, output_size):
+        self.sess = sess
+        self.net_name = name
+        self.input_size = input_size
+        self.output_size = output_size
+        self.learning_rate = 0.001
+
+        self.createNetwork()
 
     # truncate_normal: 절단정규분포로부터의 난수값을 반환합니다.
     # stddev: 0-D 텐서 또는 파이썬 값. 절단정규분포의 표준 편차.
@@ -37,49 +42,49 @@ class DQN():
         initial = tf.constant(0.01, shape = shape)
         return tf.Variable(initial)
 
-    @staticmethod
-    def conv2d(x, W, stride):
-        return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
-
-    @staticmethod
-    def max_pool_2x2(x):
-        return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
 
     def createNetwork(self):
-        W_conv1 = DQN.weight_variable([8, 8, 4, 32])
+        with tf.variable_scope(self.net_name):
+            self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
+        
+            W1 = tf.Variable(tf.truncated_normal([self.input_size, 64], stddev = 0.01), name = "w1")
+            b1 = tf.Variable(tf.constant(0.01, shape = [64]), name = "b1")
+            Layer1 = tf.nn.relu(tf.matmul(self._X, W1) + b1)
+            # Layer1 = tf.nn.relu(tf.matmul(self._X, W1))
 
-        b_conv1 = DQN.bias_variable([32])
+            W2 = tf.Variable(tf.truncated_normal([64, 32], stddev = 0.01), name = "w2")
+            # b2 = tf.Variable(tf.constant(0.01, shape = [32]), name = "b2")
+            # Layer2 = tf.nn.relu(tf.matmul(Layer1, W2) + b2)
+            Layer2 = tf.nn.relu(tf.matmul(Layer1, W2))
 
-        W_conv2 = DQN.weight_variable([4, 4, 32, 64])
-        b_conv2 = DQN.bias_variable([64])
+            W3 = tf.Variable(tf.truncated_normal([32, 16], stddev = 0.01), name = "w3")
+            b3 = tf.Variable(tf.constant(0.01, shape = [16]), name = "b3")
+            # Layer3 = tf.nn.relu(tf.matmul(Layer2, W3) + b3)
+            Layer3 = tf.nn.relu(tf.matmul(Layer2, W3))
 
-        W_conv3 = DQN.weight_variable([3, 3, 64, 64])
-        b_conv3 = DQN.bias_variable([64])
+            W4 = tf.Variable(tf.truncated_normal([16, self.output_size], stddev = 0.01), name = "w4")
+            self._QPred = tf.nn.relu(tf.matmul(Layer3, W4))
 
-        W_fc1 = DQN.weight_variable([1600, 512])
-        b_fc1 = DQN.bias_variable([512])
+            self._Y = tf.placeholder(tf.float32, shape=[None], name="_Y")
+            self._A = tf.placeholder(tf.float32, [None, self.output_size], name="_A")
+            
+            self._QValue = tf.reduce_sum(tf.multiply(self._QPred, self._A), reduction_indices=1, name="_QValue")
 
-        W_fc2 = DQN.weight_variable([512, ACTIONS])
-        b_fc2 = DQN.bias_variable([ACTIONS])
+            # loss function
+            self._loss = tf.reduce_mean(tf.square(self._Y - self._QValue))
+            # learning
+            self._train = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self._loss)
 
-        # input layer
-        s = tf.placeholder("float", [None, 80, 80, 4])
+    
+    def predict(self, state):
+        x = np.reshape(state, [1, self.input_size])
+        return self.sess.run(self._QPred, feed_dict={self._X: x})
+        
 
-        # hidden layers
-        h_conv1 = tf.nn.relu(DQN.conv2d(s, W_conv1, 4) + b_conv1)
-        h_pool1 = DQN.max_pool_2x2(h_conv1)
+    def update(self, s_batch, r_batch, a_batch):
+        return self.sess.run(self._train, feed_dict={self._X: s_batch, self._Y: r_batch, self._A: a_batch})
 
-        h_conv2 = tf.nn.relu(DQN.conv2d(h_pool1, W_conv2, 2) + b_conv2)
-        #h_pool2 = max_pool_2x2(h_conv2)
 
-        h_conv3 = tf.nn.relu(DQN.conv2d(h_conv2, W_conv3, 1) + b_conv3)
-        #h_pool3 = max_pool_2x2(h_conv3)
 
-        #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
-        h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+        
 
-        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
-
-        # readout layer
-        self.s = s
-        self.readout = tf.matmul(h_fc1, W_fc2) + b_fc2
--- MyFlappyBird/main.py
+++ MyFlappyBird/main.py
@@ -5,13 +5,106 @@
 # 
 import tensorflow as tf
 import sys
+import gym
+from collections import deque
+import random
 
 from DQN import DQN
 sys.path.append("game/")
 import numpy as np
 
-print(np.__version__)
+env = gym.make('CartPole-v1')
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
 
-a = DQN()
+discount_factor = 0.99
+epsilon = 0.1
+epsilon_decay = 0.999
+epsilon_min = 0.001
+batch_size = 64
+train_start = 200
+memory = deque(maxlen=4000)
 
-print(type(a.s))
+sess = tf.InteractiveSession()
+mainDQN = DQN(sess, "main", state_size, action_size)
+
+sess.run(tf.initialize_all_variables())
+
+def choose_action(state):
+    if (np.random.rand() <= epsilon):
+        return random.randrange(action_size)
+    else:
+        return np.argmax(mainDQN.predict(state))
+
+def remember(state, action, reward, next_state, done):
+    memory.append((state, action, reward, next_state, done))
+
+def trainModel():
+    global epsilon
+    if (epsilon > epsilon_min):
+        epsilon = epsilon * epsilon_decay
+
+    s_batch = []
+    r_stack = []
+    a_batch = []
+    
+    
+    mini_batch = random.sample(memory, batch_size)
+
+    for state, action, reward, next_state, done in mini_batch:
+        if done:
+            r_stack.append(reward)
+        else:
+            r_stack.append(reward + discount_factor * np.max(mainDQN.predict(next_state)))
+
+        s_batch.append(state)
+        a_onehot = [0, 0]
+        a_onehot[action] = 1
+        a_batch.append(a_onehot)
+
+    return mainDQN.update(s_batch, r_stack, a_batch)
+
+
+def playGame():
+
+    scores, episodes = [], []
+    score_avg = 0
+    # episode 가 시작될 때마다 환경을 초기화한다.
+    num_episode = 3000
+    for e in range(num_episode):
+        done = False
+        score = 0
+
+        state = env.reset()
+
+        # 현재 상태에서 action 을 하나 선택하여 한 스텝 진행한다.
+        # 그 결과로 받은 보상을 현재 상태와 선택한 행동과 함께 리플레이 메모리에 저장한다.
+        # 리플레이 메모리가 일정 크기 이상으로 저장되면 매 스텝마다 학습할 수 있도록 한다.
+        while not done:
+            env.render()
+
+            action = choose_action(state)
+
+            next_state, reward, done, info = env.step(action)
+
+            score += reward
+            if not done or score == 500:
+                reward = 0.1
+            else:
+                reward = -1
+
+            remember(state, action, reward, next_state, done)
+            if len(memory) >= train_start:
+                trainModel()
+
+            state = next_state
+
+            if done:
+                if score_avg == 0:
+                    score_avg = score
+                else:
+                    score_avg = 0.9 * score_avg + 0.1 * score
+                
+                print('episode: {:3d} | score avg {:3.2f} | memory length: {:4d} | epsilon: {:.4f}'.format(e, score_avg, len(memory), epsilon))
+    
+playGame()
