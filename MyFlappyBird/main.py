2ee938028520dce503367fd32a56d3dfba553c4b
DESKTOP-0RH78CA\sbh13 <sukmin.shin@playlinks.com>
2022-02-23 00:50:40 +0900

    LunarLander-V2

--- MyFlappyBird/main.py
+++ MyFlappyBird/main.py
@@ -24,23 +24,38 @@ def get_copy_var_ops(dest_scope_name="target", src_scope_name="main"):
         op_holder.append(dest_var.assign(src_var.value()))
     return op_holder
 
-env = gym.make('CartPole-v1')
+env = gym.make('LunarLander-v2')
+# env = gym.make('CartPole-v1')
+
 state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
 
+print('State shape: ', env.observation_space.shape)
+print('Number of actions: ', env.action_space.n)
+
 discount_factor = 0.99
-epsilon = 0.4
+epsilon = 0.01
 epsilon_decay = 0.999
-epsilon_min = 0.01
+epsilon_min = 0.0005
 batch_size = 64
 train_start = 200
-memory = deque(maxlen=2000)
+memory = deque(maxlen=10000)
+
+USE_CHECK_POINT = True
 
 sess = tf.InteractiveSession()
 mainDQN = DQN(sess, "main", state_size, action_size)
 targetDQN = DQN(sess, "target", state_size, action_size)
 
+saver = tf.train.Saver()
 sess.run(tf.initialize_all_variables())
+
+checkpoint = tf.train.get_checkpoint_state("saved_networks")
+if checkpoint and checkpoint.model_checkpoint_path:
+    saver.restore(sess, checkpoint.model_checkpoint_path)
+    print("Successfully loaded:", checkpoint.model_checkpoint_path)
+else:
+    print("Could not find old network weights")
 
 copy_ops = get_copy_var_ops(dest_scope_name="target", src_scope_name="main")
 
@@ -71,13 +86,11 @@ def trainModel():
             r_stack.append(reward + discount_factor * np.max(targetDQN.predict(next_state)))
 
         s_batch.append(state)
-        a_onehot = [0, 0]
+        a_onehot = np.zeros(action_size)
         a_onehot[action] = 1
         a_batch.append(a_onehot)
 
     return mainDQN.update(s_batch, r_stack, a_batch)
-
-
 
 def playGame():
 
@@ -85,6 +98,7 @@ def playGame():
     score_avg = 0
     # episode 가 시작될 때마다 환경을 초기화한다.
     num_episode = 3000
+    t = 0
     for e in range(num_episode):
         done = False
         score = 0
@@ -96,16 +110,15 @@ def playGame():
         # 리플레이 메모리가 일정 크기 이상으로 저장되면 매 스텝마다 학습할 수 있도록 한다.
         while not done:
             env.render()
+            t = t + 1
 
             action = choose_action(state)
 
             next_state, reward, done, info = env.step(action)
 
+            if done and reward < 100:
+                print('reward {}'.format(reward))
             score += reward
-            if not done or score == 500:
-                reward = 0.1
-            else:
-                reward = -1
 
             remember(state, action, reward, next_state, done)
             if len(memory) >= train_start:
@@ -121,13 +134,16 @@ def playGame():
                 else:
                     score_avg = 0.9 * score_avg + 0.1 * score
                 
-                print('episode: {:3d} | score avg {:3.2f} | memory length: {:4d} | epsilon: {:.4f}'.format(e, score_avg, len(memory), epsilon))
+                print('episode: {:3d} | score {:3.2f} | memory length: {:4d} | epsilon: {:.4f}'.format(e, score, len(memory), epsilon))
 
-                scores.append(score_avg)
+                scores.append(score)
                 episodes.append(e)
                 plt.plot(episodes, scores, 'b')
                 plt.xlabel('episode')
-                plt.ylabel('average score')
+                plt.ylabel('score')
                 plt.savefig('cartpole_graph.png')
+
+                if e % 50 == 1:
+                    saver.save(sess, 'saved_networks/cartpole-dqn')
     
 playGame()
